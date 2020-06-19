58714862e309fdea19536fbd8b4f664b28d193fa
Nathan Hansen <nhansen@ouradmincenter.com>
2020-06-19 17:59:10 -0600

    Use tailwind grid for sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,44 +1,42 @@
 <template>
-    <div class="container  grid">
-        <div class="m-3 rounded-full p-5">
+    <div class="container  grid grid-rows-7 text-white row-gap-16">
+        <div class="mt-0 mb-auto rounded-full h-16 2-16 row-span-1 row-start-2">
             <font-awesome-layers class="fa-2x">
                 <font-awesome-icon :icon="['fas', 'circle']" transform="left-3 up-3"></font-awesome-icon>
-                <font-awesome-icon :icon="['fas', 'circle']" transform="right-3 down-3" :style="{ opacity: '50%' }"></font-awesome-icon>
+                <font-awesome-icon :icon="['fas', 'circle']" transform="right-3 down-3"
+                                   :style="{ opacity: '50%' }"></font-awesome-icon>
             </font-awesome-layers>
-
-
         </div>
-        <div>
-            <div class="my-1">
-                <div class="m-3 rounded-full p-5 transition duration-500 ease-in-out hover:bg-indigo-700">
+        <div class="row-span-4">
+            <div class="my-3 mx-auto px-3">
+                <div class="rounded-full h-20 w-20 p-6 transition duration-500 ease-in-out hover:bg-indigo-700">
                     <font-awesome-icon :icon="['fal', 'list']" size="2x"></font-awesome-icon>
-
                 </div>
             </div>
-            <div class="my-1">
-                <div class="m-3 rounded-full p-5 transition duration-500 ease-in-out hover:bg-indigo-700 bg-indigo-700">
+            <div class="my-3 mx-auto px-3">
+                <div class="rounded-full h-20 w-20 p-6 transition duration-500 ease-in-out hover:bg-indigo-700 bg-indigo-700">
                     <font-awesome-icon :icon="['fal', 'clipboard']" size="2x"></font-awesome-icon>
                 </div>
             </div>
-            <div class="my-1">
-                <div class="m-3 rounded-full p-5 transition duration-500 ease-in-out hover:bg-indigo-700">
+            <div class="my-3 mx-auto px-3">
+                <div class="rounded-full h-20 w-20 p-6 transition duration-500 ease-in-out hover:bg-indigo-700">
                     <font-awesome-icon :icon="['fal', 'bell']" size="2x"></font-awesome-icon>
                 </div>
             </div>
-            <div class="my-1">
-                <div class="m-3 rounded-full p-5 transition duration-500 ease-in-out hover:bg-indigo-700">
+            <div class="my-3 mx-auto px-3">
+                <div class="rounded-full h-20 w-20 p-6 transition duration-500 ease-in-out hover:bg-indigo-700">
                     <font-awesome-icon :icon="['fal', 'envelope']" size="2x"></font-awesome-icon>
                 </div>
             </div>
-            <div class="my-1">
-                <div class="m-3 rounded-full p-5 transition duration-500 ease-in-out hover:bg-indigo-700">
+            <div class="my-3 mx-auto px-3">
+                <div class="rounded-full h-20 w-20 p-6 transition duration-500 ease-in-out hover:bg-indigo-700">
                     <font-awesome-icon :icon="['fal', 'clock']" size="2x"></font-awesome-icon>
                 </div>
             </div>
         </div>
-        <div class="m-3 rounded-full">
+        <div class="m-3 rounded-full row-span-1">
             <img src="@/assets/me.jpg"
-                 class="profile rounded-full w-full transition duration-500 ease-in-out hover:shadow-md shadow-none hover:opacity-75"
+                 class="profile rounded-full m-auto w-16 h-16 transition duration-500 ease-in-out hover:shadow-md shadow-none hover:opacity-75"
                  alt="">
         </div>
     </div>
@@ -57,8 +55,5 @@
 
     .container {
         background: #686CFF;
-        grid-template-rows: 1fr 4fr 1fr;
-        grid-gap: 8rem;
-        color: white;
     }
 </style>
