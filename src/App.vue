f2bafdf49e48ac3bf72f66a44fb88866e5c8d51e
Nathan Hansen <nhansen@ouradmincenter.com>
2020-06-19 20:55:59 -0600

    Set fixed sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,40 @@
 <template>
-  <div id="app" class="h-full w-full">
-    <SideBar></SideBar>
-    <MainContent></MainContent>
-    <RightContextMenu></RightContextMenu>
-  </div>
+    <div id="app" class="h-full w-full">
+        <div id="content">
+            <SideBar class="h-screen fixed left-0"></SideBar>
+            <MainContent class="col-start-2"></MainContent>
+            <RightContextMenu></RightContextMenu>
+        </div>
+    </div>
 </template>
 
 <script>
-import SideBar from "./components/SideBar";
-import MainContent from "./components/MainContent";
-import RightContextMenu from "./components/RightContextMenu";
-export default {
-  name: "App",
-  components: {
-    SideBar,
-    MainContent,
-    RightContextMenu
-  }
-};
+    import SideBar from "./components/SideBar";
+    import MainContent from "./components/MainContent";
+    import RightContextMenu from "./components/RightContextMenu";
+
+    export default {
+        name: "App",
+        components: {
+            SideBar,
+            MainContent,
+            RightContextMenu
+        }
+    };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  display: grid;
-  grid-template-columns: 100px 4fr 1fr;
-  grid-template-rows: 1fr;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+
+    #content {
+        display: grid;
+        grid-template-rows: 1fr;
+        grid-template-columns:100px 4fr 1fr;
+    }
 </style>
